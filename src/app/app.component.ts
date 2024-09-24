4ce7c91ecf4e459b49f27e1b38d95411dd56a0ab
glesi <glesi.mayandy@gmail.com>
2024-09-23 19:57:56 -0600

    Se agregan modulos para login, registro, agregar cuenta, ver lista de movimientos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,23 @@ import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  isSidebarActive: boolean = false;
+  isLoginPage: boolean = false;
+
   @ViewChild('btn') btn!: ElementRef;
   @ViewChild('bxSearch') bxSearch!: ElementRef;
-  constructor(private renderer: Renderer2) {}
+
+  constructor(private renderer: Renderer2, private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.isLoginPage = event.url === '/login' || event.url === '/register';
+    });
+  }
+
+  toggleSidebar() {
+    this.isSidebarActive = !this.isSidebarActive;
+  }
 
   toggleChange() {
     const sidebar = this.btn.nativeElement.closest('.sidebar');
