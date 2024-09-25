d6243d60af48c358f2149d5a425e098225273e0a
DeboraRivas1805 <00059922@uca.edu.sv>
2024-09-24 21:06:11 -0600

    Modulos de Gerentes Listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { LoanStatusComponent } from './clerk-module/loan-status/loan-status.comp
 import { ClerkSummaryComponent } from './clerk-module/clerk-summary/clerk-summary.component';
 import { AddCustomerComponent } from './clerk-module/add-customer/add-customer.component';
 import { ActionsComponent } from './clerk-module/actions/actions.component';
+import { EmployeerGeneralComponent } from './general-managment-module/employeer/employeer.component';
+import { EmployeerBranchComponent } from './branch-managment-module/employeer/employeer.component';
+import { BranchComponent } from './general-managment-module/branch/branch.component';
+import { LoanStatusBranchComponent } from './branch-managment-module/loan-status/loan-status.component';
+import { MovementsComponent } from './general-managment-module/movements/movements.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'clerk-summary', pathMatch: 'full'},
@@ -15,17 +20,29 @@ const routes: Routes = [
   {path: 'add-customer', component: AddCustomerComponent},
   {path:'req-loan', component:ReqLoanComponent},
   {path:'loan-status', component: LoanStatusComponent},
-  {path:'actions', component:ActionsComponent}
+  {path:'actions', component:ActionsComponent},
+  {path:'branch', component:BranchComponent},
+  {path:'employeer-general', component:EmployeerGeneralComponent},
+  {path:'employeer-branch', component:EmployeerBranchComponent},
+  {path:'loan-status-branch', component:LoanStatusBranchComponent},
+  {path:'movements-general', component:MovementsComponent}
+
+
 ]
   
 
 @NgModule({
   declarations: [				
-    AppComponent,
-    AddCustomerComponent,
+      AppComponent,
+      AddCustomerComponent,
       ReqLoanComponent,
       LoanStatusComponent,
-      ClerkSummaryComponent
+      ClerkSummaryComponent,
+      BranchComponent,
+      EmployeerBranchComponent,
+      EmployeerGeneralComponent,
+      LoanStatusBranchComponent,
+      MovementsComponent
    ],
   imports: [
     BrowserModule,
